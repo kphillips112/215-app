084b9789cbdb5439ca4e7dfabf169a394eae3f2b
kphillips112 <157815663+kphillips112@users.noreply.github.com>
2024-03-24 14:04:22 -0400

    Add files via upload
    Changes made

--- 215-app/src/app/app.routes.ts
+++ 215-app/src/app/app.routes.ts
@@ -1,14 +1,28 @@
 import { Routes } from '@angular/router';
 import { ProfilePageComponent } from './profile-page/profile-page.component';
 import { DetailPageComponent } from './detail-page/detail-page.component';
-import { CourseTableComponent } from './transcript-module/course-table/course-table.component';
+import { IngredientsTableComponent } from './recipe-module/ingredients-table/ingredients-table.component';
+import { IngredientDetailComponent } from './recipe-module/ingredient-detail/ingredient-detail.component';
+import { arbitraryGuard } from './site-guards.guard';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { routeGuardGuard } from './route-guard.guard';
 
 export const routes: Routes = [
-    { path: 'profile', component: ProfilePageComponent, title: 'Profile'},
-    { path: 'detail', component: DetailPageComponent, title: 'Detail', canActivate: [routeGuardGuard] },
-    { path: 'transcript', component: CourseTableComponent, title: 'Transcript' },
-    { path: '', component: ProfilePageComponent},
-    { path: '**', component: NotFoundComponent },
+  { path: 'profile', component: ProfilePageComponent, title: 'Profile' },
+  {
+    path: 'detail',
+    component: DetailPageComponent,
+    title: 'Detail',
+    canActivate: [arbitraryGuard],
+  },
+  {
+    path: 'recipes',
+    component: IngredientsTableComponent,
+    title: 'Recipes',
+    children: [
+      { path: 'ingredient-detail', component: IngredientDetailComponent },
+    ],
+  },
+  
+  { path: '', component: ProfilePageComponent },
+  { path: '**', component: NotFoundComponent },
 ];
